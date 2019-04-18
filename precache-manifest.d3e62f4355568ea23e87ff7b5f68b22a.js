7cbca29dc51f3983f59bfa946742695d6f6985ca
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2019-04-17 23:39:22 -0300

    Add files via upload

---
+++ precache-manifest.d3e62f4355568ea23e87ff7b5f68b22a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "67b63d4044e7d7a65d1d",
+    "url": "/static/js/main.67b63d40.chunk.js"
+  },
+  {
+    "revision": "10cf4b098224b5463664",
+    "url": "/static/js/2.10cf4b09.chunk.js"
+  },
+  {
+    "revision": "67b63d4044e7d7a65d1d",
+    "url": "/static/css/main.42ebebc6.chunk.css"
+  },
+  {
+    "revision": "7642be469ff9440527e8a7703a438cc8",
+    "url": "/index.html"
+  }
+];
